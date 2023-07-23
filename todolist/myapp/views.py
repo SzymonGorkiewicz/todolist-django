1e858f5267b6d9f1b87a69237598e71f3098ca28
repoSZ <s.gorkiewicz@interia.pl>
2023-07-23 19:05:49 +0200

    added navbar

--- todolist/myapp/views.py
+++ todolist/myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic.edit import CreateView
 # Create your views here.
-
+from .models import ToDoList
+from .forms import CreateList
 
 def home(request):
     return render(request, "home.html")
+
+
+def create(request):
+    return render(request, "create.html")

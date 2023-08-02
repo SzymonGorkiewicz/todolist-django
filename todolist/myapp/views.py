fdd4a26fc194b30ac729bc432f4efd7796247e7e
repoSZ <s.gorkiewicz@interia.pl>
2023-08-02 16:28:20 +0200

    added json data initializer

--- todolist/myapp/views.py
+++ todolist/myapp/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 
 @login_required()

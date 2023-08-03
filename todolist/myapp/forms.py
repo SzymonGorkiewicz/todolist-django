64004c351834611fc9c62bcbf1c594b7d5058c50
repoSZ <s.gorkiewicz@interia.pl>
2023-08-03 16:33:48 +0200

    fixed errrors

--- todolist/myapp/forms.py
+++ todolist/myapp/forms.py
@@ -11,7 +11,7 @@ class CreateList(forms.ModelForm):
 class CreateUser(UserCreationForm):
     class Meta:
         model=User
-        fields=['username', 'email', 'password1', 'password2']
+        fields=['username', 'password1', 'password2']
 
 class CreateItem(forms.ModelForm):
     class Meta:
